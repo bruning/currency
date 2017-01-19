2a2d64265f8e76a674a93d3b03585bbf6d7c64cc
bruning <ben@automatedinsights.com>
2017-01-19 09:44:26 -0500

    Add files via upload

---
+++ currency.rb
@@ -0,0 +1,12 @@
+class Currency
+  attr_reader :amount, :currency_code
+
+  def initialize(amount:, currency_code:)
+    @amount = amount
+    @currency_code = currency_code
+  end
+
+  def ==(other)
+    @amount == other.amount && @currency_code == other.currency_code
+  end
+end
